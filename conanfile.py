de74461fa412dbad74f834a0a3395e1fe7995058
Shinichi Katayama <shin1.katayama@gmail.com>
2016-09-08 14:18:45 +0900

    Support Windows

--- conanfile.py
+++ conanfile.py
@@ -12,9 +12,8 @@ class NodeConan( ConanFile ):
   settings = 'os', 'compiler', 'build_type', 'arch'
   options = { 'shared': [ True, False ] }
   default_options = 'shared=True'
-  generators = 'cmake'
-  folder = '%s-%s-%s' % ( name, name, version )
   source_dir = '%s-%s' % ( name, version )
+  short_paths=True
 
   def source( self ):
     zip_name = '%s.tar.gz' % ( self.version )
@@ -23,20 +22,35 @@ class NodeConan( ConanFile ):
     os.unlink( zip_name )
 
   def build( self ):
-    env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
-    flags = '--enable-shared' if self.options.shared else ''
-    if self.settings.build_type == 'Debug':
-      flags += ' --debug'
-    configure_command = 'cd %s && %s ./configure %s' % ( self.source_dir, env.command_line, flags )
-    self.output.info( 'Configure with: %s' % configure_command )
-    self.run( configure_command )
-    self.run( 'cd %s && %s make -j2' % ( self.source_dir, env.command_line) )
+    if self.settings.os == 'Windows':
+      flags = 'nosign'
+      if self.options.shared:
+        flags += ' shared'
+      if self.settings.build_type == 'Debug':
+        flags += ' debug'
+      if self.settings.arch == 'x86':
+        flags += ' x86'
+      elif self.settings.arch == 'x86_64':
+        flags += ' x64'
+      build_command = 'cd %s & vcbuild.bat %s' % ( self.source_dir, flags )
+      self.output.info( 'build_command: %s' % build_command )
+      self.run( build_command )
+    else:
+      env = ConfigureEnvironment(self.deps_cpp_info, self.settings)
+      flags = '--enable-shared' if self.options.shared else ''
+      if self.settings.build_type == 'Debug':
+        flags += ' --debug'
+      configure_command = 'cd %s && %s ./configure %s' % ( self.source_dir, env.command_line, flags )
+      self.output.info( 'Configure with: %s' % configure_command )
+      self.run( configure_command )
+      self.run( 'cd %s && %s make -j2' % ( self.source_dir, env.command_line) )
 
   def package( self ):
     self.copy( '*.h', dst='include', src='%s/src' % self.source_dir )
     self.copy( '*.h', dst='deps/v8', src='%s/deps/v8' % self.source_dir )
     self.copy( '*.h', dst='deps/v8/include', src='%s/deps/v8/include' % self.source_dir )
     self.copy( '*.h', dst='deps/uv/include', src='%s/deps/uv/include' % self.source_dir )
+    self.copy( '*.h', dst='deps/uv/src', src='%s/deps/uv/src' % self.source_dir )
     self.copy( '*.h', dst='deps/cares/include', src='%s/deps/cares/include' % self.source_dir )
     self.copy( '*.h', dst='deps/openssl/openssl/include', src='%s/deps/openssl/openssl/include' % self.source_dir )
     self.copy( '*.h', dst='deps/openssl/openssl/crypto', src='%s/deps/openssl/openssl/crypto' % self.source_dir )
@@ -54,11 +68,4 @@ class NodeConan( ConanFile ):
 
   def package_info( self ):
     self.cpp_info.includedirs = ['include', 'deps/v8', 'deps/v8/include', 'deps/uv/include', 'deps/cares/include', 'deps/openssl/openssl/include']
-
-    if self.settings.os == 'Windows':
-      if self.settings.build_type == 'Debug':
-        self.cpp_info.libs = [ 'libnodemdd' ]
-      else:
-        self.cpp_info.libs = [ 'libnodemd' ]
-    else:
-      self.cpp_info.libs = [ 'node' ]
+    self.cpp_info.libs = [ 'node' ]
